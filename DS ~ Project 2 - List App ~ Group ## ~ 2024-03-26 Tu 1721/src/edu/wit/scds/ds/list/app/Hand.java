f9813c194ca0f1c4c4e015da50151245d0109438
maloofkatwit <144467152+maloofkatwit@users.noreply.github.com>
2024-04-01 12:13:55 -0400

    Initial

---
+++ DS ~ Project 2 - List App ~ Group ## ~ 2024-03-26 Tu 1721/src/edu/wit/scds/ds/list/app/Hand.java
@@ -0,0 +1,52 @@
+/* @formatter:off
+ *
+ * Dave Rosenberg
+ * Comp 2000 - Data Structures
+ * Lab: List application - card game
+ * Spring, 2024
+ * 
+ * Usage restrictions:
+ * 
+ * You may use this code for exploration, experimentation, and furthering your
+ * learning for this course. You may not use this code for any other
+ * assignments, in my course or elsewhere, without explicit permission, in
+ * advance, from myself (and the instructor of any other course).
+ * 
+ * Further, you may not post (including in a public repository such as on github)
+ * nor otherwise share this code with anyone other than current students in my 
+ * sections of this course. Violation of these usage restrictions will be considered 
+ * a violation of the Wentworth Institute of Technology Academic Honesty Policy.
+ *
+ * Do not remove this notice.
+ *
+ * @formatter:on
+ */
+
+
+package edu.wit.scds.ds.list.app ;
+
+/**
+ * Representation of a hand of cards
+ *
+ * @author Your Name    // TODO
+ *
+ * @version 1.0.0 2024-03-26 Initial implementation
+ */
+public class Hand extends Pile
+    {
+    // TODO implement this
+
+
+    /**
+     * (optional) test driver
+     *
+     * @param args
+     *     -unused-
+     */
+    public static void main( final String[] args )
+        {
+        // OPTIONAL for testing and debugging
+
+        }	// end main()
+
+    }	// end class Hand
