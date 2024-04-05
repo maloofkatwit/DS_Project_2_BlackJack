5731c13dfc634357b2015093dabc94cc71fe9ccb
maguiretatwit <144469528+maguiretatwit@users.noreply.github.com>
2024-04-05 14:24:40 -0400

    Update BlackJack.java

--- DS ~ Project 2 - List App ~ Group ## ~ 2024-03-26 Tu 1721/src/edu/wit/scds/ds/list/app/BlackJack.java
+++ DS ~ Project 2 - List App ~ Group ## ~ 2024-03-26 Tu 1721/src/edu/wit/scds/ds/list/app/BlackJack.java
@@ -1,3 +1,7 @@
+/*
+ * Tyger Maguire Comp 2000 - Data Structures Lab: List application - card game
+ * Spring, 2024
+ */
 
 package edu.wit.scds.ds.list.app ;
 
@@ -14,14 +18,18 @@ public class BlackJack
     {
 
     /**
+     * Main method that runs through the full Game.
+     * 
      * @param args
      */
     public static void main( String[] args )
         {
+        // create list of players deck for the game and the dealer
         List<Player> pl = new ArrayList<Player>() ;
         Deck gameDeck = new Deck() ;
         Player dealer = new Player( "Dealer" ) ;
 
+        // asks users for number of players that will be joining
         Scanner game = new Scanner( System.in ) ;  // Create a Scanner object
         System.out.println( "How many will be playing today: " ) ;
         char playerNumber = 'N' ;
@@ -49,6 +57,7 @@ public class BlackJack
 
             }
 
+        // asks users for name of all players
         for ( int i = 0 ; i < playerNumber - '0' ; i++ )
             {
             System.out.println( "please input a player name" ) ;
@@ -58,11 +67,14 @@ public class BlackJack
 
             }
 
+        // game loop begins
         boolean gamePlay = true ;
         Card hiddenCard = null ;
         while ( gamePlay )
             {
+            // shuffle deck
             gameDeck.shuffle() ;
+            // asks each player to input there bet number
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 firstTime = true ;
@@ -95,20 +107,25 @@ public class BlackJack
 
                 }
 
+            // adds first card to player hand
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 pl.get( i ).addCard( 0, gameDeck.draw() ) ;
 
                 }
 
+            // adds first card to dealer hand
             dealer.addCard( gameDeck.draw() ) ;
+            // adds second card to player hand
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 pl.get( i ).addCard( 0, gameDeck.draw() ) ;
 
                 }
 
+            // add second hidden card to dealer hand
             hiddenCard = ( gameDeck.draw() ) ;
+            // shows hands of all players and dealers
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 System.out.println( pl.get( i ).getName() + " hand: " + pl.get( i ).toString() ) ;
@@ -120,7 +137,7 @@ public class BlackJack
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 boolean doubled = false ;
-
+                // check if player got a natural blackjack
                 if ( pl.get( i ).getHand( 0 ).calculateScore() == 21 )
                     {
                     System.out.println( "congradualtions " + pl.get( i ).getName() +
@@ -129,7 +146,8 @@ public class BlackJack
                     pl.get( i ).increaseBalance( (int) ( pl.get( i ).getBet( 0 ) * 1.5 ) ) ;
 
                     }
-
+                // checks if player can split and if they would like to then splits
+                // there hand if they would
                 else if ( pl.get( i ).canSplit() )
                     {
                     System.out.println( pl.get( i ).getName() + ", would you like to split?" ) ;
@@ -156,7 +174,8 @@ public class BlackJack
                         }
 
                     }
-
+                // checks if player can double down and if they want to. doubles down
+                // if they do
                 else if ( ( pl.get( i ).getHand( 0 ).calculateScore() == 9 ||
                             pl.get( i ).getHand( 0 ).calculateScore() == 10 ||
                             pl.get( i ).getHand( 0 ).calculateScore() == 11 ) &&
@@ -196,7 +215,7 @@ public class BlackJack
 
                     firstTime = true ;
                     boolean turn = true ;
-
+                    // check if player has a blackjack
                     while ( turn )
                         {
                         System.out.println( pl.get( i ).getName() + ": " +
@@ -208,17 +227,21 @@ public class BlackJack
                             turn = false ;
 
                             }
+                        // ends players turn after one run if they doubled
                         else if ( doubled )
                             {
                             turn = false ;
 
                             }
+                        // checks if player has busted
                         else if ( pl.get( i ).getHand( ii ).isBusted() )
                             {
                             System.out.println( pl.get( i ).getName() + " has busted" ) ;
                             turn = false ;
 
                             }
+                        // asks player if they want to hit or stand. ends there turn
+                        // if they stand
                         else
                             {
                             System.out.println( pl.get( i ).getName() +
@@ -234,7 +257,8 @@ public class BlackJack
                                     }
 
                                 answer = game.nextLine() ;
-                                firstTime=false;
+                                firstTime = false ;
+
                                 }
 
                             if ( answer.equals( "hit" ) )
@@ -258,9 +282,11 @@ public class BlackJack
 
                 }
 
+            // adds hidden card to dealers hand
             boolean dealerTurn = true ;
             dealer.addCard( hiddenCard ) ;
-            while ( dealerTurn)
+            // adds card to dealers hand untill there over 17
+            while ( dealerTurn )
                 {
                 System.out.println( "dealer hand: " + dealer.getHand( 0 ).toString() ) ;
                 if ( dealer.getHand( 0 ).calculateScore() < 17 )
@@ -276,15 +302,19 @@ public class BlackJack
 
                 }
 
+            // checks if players have won lost or tied
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 for ( int ii = 0 ; ii < pl.get( i ).getHands().size() ; ii++ )
                     {
+                    // checks if players hand has won
                     if ( ( pl.get( i ).getHand( ii ).calculateScore() >
                            dealer.getHand( 0 ).calculateScore() ||
                            dealer.getHand( 0 ).calculateScore() > 21 ) &&
                          pl.get( i ).getHand( ii ).calculateScore() <= 21 )
                         {
+                        // checks if player got blackjack and give player there
+                        // points
                         if ( pl.get( i ).getHand( ii ).calculateScore() == 21 )
                             {
                             System.out.println( pl.get( i ).getName() + "has won " +
@@ -297,6 +327,7 @@ public class BlackJack
                                                 pl.get( i ).getBalance() + " points remainig " ) ;
 
                             }
+                        // gives player there points
                         else
                             {
                             System.out.println( pl.get( i ).getName() + "has won " +
@@ -311,6 +342,8 @@ public class BlackJack
                             }
 
                         }
+                    // checks if player and dealer have tied and gives player there
+                    // points
                     else if ( ( pl.get( i ).getHand( ii ).calculateScore() ==
                                 dealer.getHand( 0 ).calculateScore() &&
                                 pl.get( i ).getHand( ii ).calculateScore() <= 21 ) ||
@@ -323,7 +356,7 @@ public class BlackJack
                                             pl.get( i ).getBalance() + " points remaining " ) ;
 
                         }
-
+                    // takes lost points away form player
                     else
                         {
                         System.out.println( pl.get( i ).getName() + "has lost " +
@@ -337,6 +370,7 @@ public class BlackJack
 
                 }
 
+            // re adds cards from players hands back to deck and resets players hand
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 for ( int ii = 0 ; ii < pl.get( i ).getHands().size() ; ii++ )
@@ -347,6 +381,7 @@ public class BlackJack
 
                 }
 
+            // re adds cards from dealers hand back to deck and resets dealers hand
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
 
@@ -356,6 +391,9 @@ public class BlackJack
 
             gameDeck.addCards( dealer.getHand( 0 ).getCards() ) ;
             dealer.resetHand() ;
+
+            // checks if anyone has run out of points and if they have removes them
+            // form the game
             for ( int i = pl.size() - 1 ; i >= 0 ; i-- )
                 {
                 if ( pl.get( i ).getBalance() <= 0 )
@@ -368,6 +406,8 @@ public class BlackJack
 
                 }
 
+            // asks if players would like to leave the game or not and removes them
+            // if they do
             for ( int i = pl.size() - 1 ; i >= 0 ; i-- )
                 {
                 System.out.println( pl.get( i ).getName() +
@@ -394,6 +434,7 @@ public class BlackJack
 
                 }
 
+            // shows remaining players
             String remainingPlayers = "the remaing players are: " ;
             for ( int i = 0 ; i < pl.size() ; i++ )
                 {
@@ -401,12 +442,14 @@ public class BlackJack
 
                 }
 
+            // checks if there are any players left and ends game if there aren't
             if ( pl.size() == 0 )
                 {
                 System.out.println( "No more players remain, game has ended" ) ;
                 gamePlay = false ;
 
                 }
+            //shows remaining players
             else
                 {
                 System.out.println( remainingPlayers ) ;
@@ -414,7 +457,7 @@ public class BlackJack
                 }
 
             }
-
+        //ends game
         System.out.println( "Thanks for playing" ) ;
         game.close() ;
 
