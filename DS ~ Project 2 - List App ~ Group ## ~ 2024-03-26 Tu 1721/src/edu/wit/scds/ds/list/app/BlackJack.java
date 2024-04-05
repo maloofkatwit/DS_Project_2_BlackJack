12c784aecd8dd90fffb0423d81a9e7b9b888b4e9
maguiretatwit <144469528+maguiretatwit@users.noreply.github.com>
2024-04-05 09:49:37 -0400

    working main method

--- DS ~ Project 2 - List App ~ Group ## ~ 2024-03-26 Tu 1721/src/edu/wit/scds/ds/list/app/BlackJack.java
+++ DS ~ Project 2 - List App ~ Group ## ~ 2024-03-26 Tu 1721/src/edu/wit/scds/ds/list/app/BlackJack.java
@@ -1,8 +1,6 @@
 
 package edu.wit.scds.ds.list.app ;
 
-import java.util.ArrayList ;
-
 /**
  * @author Tyger Maguire
  * 
@@ -11,19 +9,23 @@ import java.util.ArrayList ;
 
 import java.util.List ;
 import java.util.Scanner ;
+import java.util.ArrayList ;
 
 public class BlackJack
     {
 
-    protected List<Player> pl ;
-    private Deck gameDeck ;
-    private Player dealer ;
+    /**
+     * 
+     */
 
     /**
      * @param args
      */
-    public void main( String[] args )
+    public static void main( String[] args )
         {
+        List<Player> pl = new ArrayList<Player>() ;
+        Deck gameDeck = new Deck() ;
+        Player dealer = new Player( "Dealer" ) ;
 
         Scanner Game = new Scanner( System.in ) ;  // Create a Scanner object
         System.out.println( "How many will be playing today: " ) ;
@@ -54,21 +56,22 @@ public class BlackJack
 
         for ( int i = 0 ; i < playerNumber - '0' ; i++ )
             {
+            System.out.println( "please input a player name" ) ;
             String playerName = Game.nextLine() ;
 
-            this.pl.add( new Player( playerName + " " ) ) ;
+            pl.add( new Player( playerName + " " ) ) ;
 
             }
 
         boolean gamePlay = true ;
-
+        Card hiddenCard = null ;
         while ( gamePlay )
             {
-            this.gameDeck.shuffle() ;
-            for ( int i = 0 ; i < this.pl.size() ; i++ )
+            gameDeck.shuffle() ;
+            for ( int i = 0 ; i < pl.size() ; i++ )
                 {
                 firstTime = true ;
-                System.out.println( this.pl.get( i ).getName() + " please make your bet." ) ;
+                System.out.println( pl.get( i ).getName() + " please make your bet." ) ;
                 int playerBet = 0 ;
                 while ( playerBet < 1 )
                     {
@@ -93,115 +96,232 @@ public class BlackJack
 
                     }
 
-                this.pl.get( i ).placeBet( 0, playerBet ) ;
+                pl.get( i ).placeBet( 0, playerBet ) ;
 
                 }
 
-            for ( int i = 0 ; i < this.pl.size() ; i++ )
+            System.out.println( pl.size() ) ;
+            for ( int i = 0 ; i < pl.size() ; i++ )
                 {
-                this.pl.get( i ).addCard( this.gameDeck.draw() ) ;
+                pl.get( i ).addCard( 0, gameDeck.draw() ) ;
 
                 }
 
-            this.dealer.addCard( this.gameDeck.draw() ) ;
-            for ( int i = 0 ; i < this.pl.size() ; i++ )
+            dealer.addCard( gameDeck.draw() ) ;
+            for ( int i = 0 ; i < pl.size() ; i++ )
                 {
-                this.pl.get( i ).addCard( this.gameDeck.draw() ) ;
+                pl.get( i ).addCard( 0, gameDeck.draw() ) ;
 
                 }
 
-            Card hiddenCard = ( this.gameDeck.draw() ) ;
-            for ( int i = 0 ; i < this.pl.size() ; i++ )
+            hiddenCard = ( gameDeck.draw() ) ;
+            for ( int i = 0 ; i < pl.size() ; i++ )
                 {
-                System.out.println( this.pl.get( i ).getName() + " hand:" ) ;
-                this.pl.get( i ).toString() ;
+                System.out.println( pl.get( i ).getName() + " hand: " + pl.get( i ).toString() ) ;
 
                 }
 
-            System.out.println( "Dealer hand: " ) ;
-            this.dealer.toString() ;
+            System.out.println( "Dealer hand: " + dealer.toString() ) ;
 
-            }
-
-        for ( int i = 0 ; i < this.pl.size() ; i++ )
-            {
-            if ( this.pl.get( i ).getHand( 0 ).calculateScore() == 21 )
+            for ( int i = 0 ; i < pl.size() ; i++ )
                 {
-                System.out.println( "congradualtions " + this.pl.get( i ).getName() +
-                                    " on your black jack you have been rewarded with " +
-                                    this.pl.get( i ).getBet( 0 ) * 1.5 + " points." ) ;
-                this.pl.get( i ).increaseBalance( (int) ( this.pl.get( i ).getBet( 0 ) * 1.5 ) ) ;
-
-                }
-
-            if ( this.pl.get( i ).canSplit() )
-                {
-                System.out.println( this.pl.get( i ).getName() + ", would you like to split?" ) ;
-                String answer = "T" ;
-                firstTime = true ;
-                while ( answer != "y" || answer != "n" )
+                boolean doubled = false ;
+                
+                if ( pl.get( i ).getHand( 0 ).calculateScore() == 21 )
                     {
-                    if ( !firstTime )
+                    System.out.println( "congradualtions " + pl.get( i ).getName() +
+                                        " on your black jack you have been rewarded with " +
+                                        pl.get( i ).getBet( 0 ) * 1.5 + " points." ) ;
+                    pl.get( i ).increaseBalance( (int) ( pl.get( i ).getBet( 0 ) * 1.5 ) ) ;
+
+                    }
+
+                else if ( pl.get( i ).canSplit() )
+                    {
+                    System.out.println( pl.get( i ).getName() + ", would you like to split?" ) ;
+                    String answer = "T" ;
+                    firstTime = true ;
+                    while ( !answer.equals( "y" ) && !answer.equals( "n" ) )
                         {
-                        System.out.println( "Invalid player input" ) ;
+                        if ( !firstTime )
+                            {
+                            System.out.println( "Invalid player input" ) ;
+
+                            }
+
+                        answer = Game.nextLine() ;
+                        firstTime = false ;
 
                         }
 
-                    answer = Game.nextLine() ;
-
-                    }
-
-                if ( answer == "y" )
-                    {
-                    this.pl.get( i ).splitHand() ;
-                    this.pl.get( i ).placeBet( 0, this.pl.get( i ).getBet( 0 ) ) ;
-
-                    }
-
-                }
-
-            if ( this.pl.get( i ).getHand( 0 ).calculateScore() == 9 ||
-                 this.pl.get( i ).getHand( 0 ).calculateScore() == 10 ||
-                 this.pl.get( i ).getHand( 0 ).calculateScore() == 11 )
-                {
-                System.out.println( this.pl.get( i ).getName() +
-                                    ", would you like to double down?" ) ;
-                String answer = "T" ;
-                firstTime = true ;
-                while ( answer != "y" || answer != "n" )
-                    {
-                    if ( !firstTime )
+                    if ( answer.equals(  "y") )
                         {
-                        System.out.println( "Invalid player input" ) ;
+                        pl.get( i ).splitHand() ;
+                        pl.get( i ).placeBet( 0, pl.get( i ).getBet( 0 ) ) ;
 
                         }
 
-                    answer = Game.nextLine() ;
-
                     }
 
-                if ( answer == "y" )
+                else if ( pl.get( i ).getHand( 0 ).calculateScore() == 9 ||
+                          pl.get( i ).getHand( 0 ).calculateScore() == 10 ||
+                          pl.get( i ).getHand( 0 ).calculateScore() == 11 )
                     {
-                    this.pl.get( i ).doubleDown( 0, gameDeck.draw() ) ;
-                    ;
+                    System.out.println( pl.get( i ).getName() +
+                                        ", would you like to double down?" ) ;
+                    String answer = "T" ;
+                    firstTime = true ;
+                    while ( !answer.equals( "y" ) && !answer.equals( "n" ) )
+                        {
+                        if ( !firstTime )
+                            {
+                            System.out.println( "Invalid player input" ) ;
+
+                            }
+
+                        answer = Game.nextLine() ;
+
+                        }
+
+                    if ( answer.equals( "y" ) )
+                        {
+                        pl.get( i ).doubleDown( 0, gameDeck.draw() ) ;
+                        doubled = true ;
+
+                        }
+
+                    }
+                for ( int ii = 0 ; ii < pl.get( i ).getHands().size() ; ii++ )
+                    {
+                    //System.out.println(pl.get( i ).getHands().size() );
+
+                    firstTime = true ;
+                    boolean turn = true ;
+
+                    while ( turn )
+                        {
+                        System.out.println( pl.get( i ).getHand( ii ).toString() ) ;
+
+                        if ( firstTime && pl.get( i ).getHand( i ).calculateScore() == 21 )
+                            {
+
+                            turn = false ;
+
+                            }
+                        else if ( doubled == true )
+                            {
+                            turn = false ;
+
+                            }
+                        else if ( pl.get( i ).getHand( ii ).isBusted() )
+                            {
+                            Syst
+                            turn = false ;
+
+                            }
+                        else
+                            {
+                            System.out.println( pl.get( i ).getName() +
+                                                ", would you like to hit or stand?" ) ;
+                            String answer = "T" ;
+                            firstTime = true ;
+                            while ( !answer.equals( "hit" ) && !answer.equals( "stand" ) )
+                                {
+                                if ( !firstTime )
+                                    {
+                                    System.out.println( "Invalid player input" ) ;
+
+                                    }
+
+                                answer = Game.nextLine() ;
+
+                                }
+
+                            if ( answer.equals ("hit") )
+                                {
+                                pl.get( i ).hit( ii, gameDeck.draw() ) ;
+
+                                }
+                            else if ( answer.equals ("stand") )
+                                {
+                                turn = false ;
+
+                                }
+
+                            }
+
+                        firstTime = false ;
+
+                        }
 
                     }
 
                 }
 
-            for ( int i = 0 ; i < this.pl.size() ; i++ )
+            boolean dealerTurn = true ;
+            dealer.addCard( hiddenCard ) ;
+            while ( dealerTurn == true )
                 {
-                boolean turn = true ;
-                firstTime=true;
-                while ( turn )
+                System.out.println( dealer.getHand( 0 ).toString() ) ;
+                if ( dealer.getHand( 0 ).calculateScore() < 17 )
                     {
-                        if(firstTime&&this.pl.get( i ).getHand( 0 ).calculateScore() == 21) {
-                        
-                        break;
+                    dealer.hit( 0, gameDeck.draw() ) ;
+
+                    }
+                else
+                    {
+                    dealerTurn = false ;
+
+                    }
+
+                }
+
+            for ( int i = 0 ; i < pl.size() ; i++ )
+                {
+                for ( int ii = 0 ; ii < pl.get( i ).getHands().size() ; ii++ )
+                    {
+                    if ( pl.get( i ).getHand( ii ).calculateScore() >
+                         dealer.getHand( 0 ).calculateScore() )
+                        {
+                        if ( pl.get( i ).getHand( ii ).calculateScore() == 21 )
+                            {
+                            pl.get( i )
+                              .increaseBalance( (int) ( pl.get( i ).getBet( ii ) +
+                                                        pl.get( i ).getBet( ii ) * 1.5 ) ) ;
+
+                            }
+                        else
+                            {
+                            pl.get( i )
+                              .increaseBalance( (int) ( pl.get( i ).getBet( ii ) +
+                                                        pl.get( i ).getBet( ii ) ) ) ;
+
+                            }
+
                         }
-                        
-                        
-                        firstTime=false;
+
+                    }
+
+                }
+
+            for ( int i = 0 ; i < pl.size() ; i++ )
+                {
+                for ( int ii = 0 ; ii < pl.get( i ).getHands().size() ; ii++ )
+                    {
+                    gameDeck.addCards( pl.get( i ).getHand( ii ).getCards() ) ;
+
+                    }
+
+                }
+
+            gameDeck.addCards( dealer.getHand( 0 ).getCards() ) ;
+
+            for ( int i = pl.size() - 1 ; i >= 0 ; i-- )
+                {
+                if ( pl.get( i ).getBalance() <= 0 )
+                    {
+                    pl.remove( i ) ;
+
                     }
 
                 }
